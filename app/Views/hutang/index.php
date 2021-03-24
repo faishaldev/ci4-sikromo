77b742f9e0018e1eff1d058abe99d4acdb7afb69
Musthafa Faishal <musthafafaishal@gmail.com>
2021-03-24 13:35:36 +0700

    update

--- app/Views/hutang/index.php
+++ app/Views/hutang/index.php
@@ -45,7 +45,7 @@
                                     <td scope="row"><?= $i++; ?></td>
                                     <td><?= $h['tgl_hutang']; ?></td>
                                     <td><?= $h['penghutang']; ?></td>
-                                    <td><?= $h['jumlah']; ?></td>
+                                    <td>Rp<?= number_format($h['jumlah'], 2, ',', .); ?></td>
                                     <td><?= $h['alasan']; ?></td>
                                     <td>text</td>
                                 </tr>
