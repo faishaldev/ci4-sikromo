28fe07b642689d291267956b49cd03b5766582ae
Musthafa Faishal <musthafafaishal@gmail.com>
2021-04-21 05:22:23 +0700

    update

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -7,6 +7,12 @@
     </a>
     <!-- Akhir Judul -->
 
+    <!-- Tombol Navigasi -->
+    <button class="navbar-toggler position-static d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <!-- Akhir Tombol Navigasi -->
+
     <!-- Tombol Sign Out -->
     <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
