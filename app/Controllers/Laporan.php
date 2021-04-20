f977a57d1f1424a9ad55df845ec053fc180a279b
Musthafa Faishal <musthafafaishal@gmail.com>
2021-04-21 01:55:50 +0700

    set timezone for export name

--- app/Controllers/Laporan.php
+++ app/Controllers/Laporan.php
@@ -70,6 +70,7 @@ class Laporan extends BaseController
         $dompdf->render();
 
         // Output the generated PDF to Browser
+        date_default_timezone_set("Asia/Bangkok");
         $dompdf->stream(date("d-m-Y") . ' ' . "Income Statement CV Romo.pdf");
     }
 }
