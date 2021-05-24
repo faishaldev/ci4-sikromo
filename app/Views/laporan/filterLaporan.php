d20bd064125f270c53143de2db261596aaf3a971
Musthafa Faishal <musthafafaishal@gmail.com>
2021-05-24 16:49:38 +0700

    Fixed laporan bulanan

--- app/Views/laporan/filterLaporan.php
+++ app/Views/laporan/filterLaporan.php
@@ -12,54 +12,53 @@
             <!-- Akhir Sub Judul -->
 
             <?php
-            $filter_tipe = @$_GET['harian'];
-            $filter_tipe = @$_GET['bulanan'];
+            $filter_tipe = @$_GET['periode'];
             $mulai_bulan = @$_GET['mulai_bulan'];
             $sampai_bulan = @$_GET['sampai_bulan'];
             $mulai_tanggal = @$_GET['mulai_tanggal'];
             $sampai_tanggal = @$_GET['sampai_tanggal'];
             ?>
 
-            <!-- Filter Laporan -->
+
             <div class="container align-items-center">
-                <div class="row">
-                    <div class="col form-group">
-                        <select name="periode" id="inputPeriode" class="form-select">
-                            <option value="">--Pilih Periode--</option>
-                            <option value="harian">Harian</option>
-                            <option value="bulanan">Bulanan</option>
-                        </select>
-                    </div>
-                </div>
-                <form name="harian" id="harian" action="laporan" method="GET" style="display: none;">
+                <form action="laporan" method="GET">
                     <div class="row">
                         <div class="col form-group">
-                            <label for="inputMulaiTanggal" class="font-weight-bold">Mulai Tanggal :</label>
-                            <input type="date" id="inputMulaiTanggal" class="form-control" name="mulai_tanggal" value="<?php echo $mulai_tanggal ?>" required>
+                            <select name="periode" id="inputPeriode" class="form-select" required>
+                                <option value="">--Pilih Periode--</option>
+                                <option value="harian" <?php echo ($filter_tipe == 'harian' ? 'selected' : ''); ?>>Harian</option>
+                                <option value="bulanan" <?php echo ($filter_tipe == 'bulanan' ? 'selected' : ''); ?>>Bulanan</option>
+                            </select>
                         </div>
-                        <div class="col form-group">
-                            <label for="inputSampaiTanggal" class="font-weight-bold">Sampai Tanggal :</label>
-                            <input type="date" id="inputSampaiTanggal" class="form-control" name="sampai_tanggal" value="<?php echo $sampai_tanggal ?>" required>
-                        </div>
-                        <button type="submit" class="col btn btn-success mt-3">Tampilkan</button>
                     </div>
-                </form>
-                <form name="bulanan" id="bulanan" action="laporan" method="GET" style="display: none;">
-                    <div class="row">
-                        <div class="col form-group">
-                            <label for="inputMulaiBulan" class="font-weight-bold">Mulai Bulan :</label>
-                            <input type="month" id="inputMulaiBulan" class="form-control" name="mulai_bulan" value="<?php echo $mulai_bulan ?>" required>
+                    <div id="harian" style="<?php echo ($filter_tipe == 'harian' ? 'display: block;' : 'display: none;'); ?>">
+                        <div class="row">
+                            <div class="col form-group">
+                                <label for="inputMulaiTanggal" class="font-weight-bold">Mulai Tanggal :</label>
+                                <input type="date" id="inputMulaiTanggal" class="form-control" name="mulai_tanggal" value="<?php echo $mulai_tanggal ?>">
+                            </div>
+                            <div class="col form-group">
+                                <label for="inputSampaiTanggal" class="font-weight-bold">Sampai Tanggal :</label>
+                                <input type="date" id="inputSampaiTanggal" class="form-control" name="sampai_tanggal" value="<?php echo $sampai_tanggal ?>">
+                            </div>
+                            <button type="submit" class="col btn btn-success mt-3">Tampilkan</button>
                         </div>
-                        <div class="col form-group">
-                            <label for="inputSampaiTanggal" class="font-weight-bold">Sampai Bulan :</label>
-                            <input type="month" id="inputSampaiBulan" class="form-control" name="sampai_bulan" value="<?php echo $sampai_bulan ?>" required>
+                    </div>
+                    <div id="bulanan" style="<?php echo ($filter_tipe == 'bulanan' ? 'display: block;' : 'display: none;'); ?>">
+                        <div class="row">
+                            <div class="col form-group">
+                                <label for="inputMulaiBulan" class="font-weight-bold">Mulai Bulan :</label>
+                                <input type="month" id="inputMulaiBulan" class="form-control" name="mulai_bulan" value="<?php echo $mulai_bulan ?>">
+                            </div>
+                            <div class="col form-group">
+                                <label for="inputSampaiTanggal" class="font-weight-bold">Sampai Bulan :</label>
+                                <input type="month" id="inputSampaiBulan" class="form-control" name="sampai_bulan" value="<?php echo $sampai_bulan ?>">
+                            </div>
+                            <button type="submit" class="col btn btn-success mt-3">Tampilkan</button>
                         </div>
-                        <button type="submit" class="col btn btn-success mt-3">Tampilkan</button>
                     </div>
                 </form>
             </div>
-            <!-- Akhir Filter Laporan -->
-
         </div>
     </div>
 </div>
--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -39,10 +39,26 @@
     <?= $this->include('layout/datatables/datatablesLaporan'); ?>
     <?= $this->include('layout/chart/barChart'); ?>
     <?= $this->include('layout/chart/doughnutChart'); ?>
+
     <script>
         $("select").on("change", function() {
             $('#' + $(this).val()).show('form').siblings('form').hide();
         })
+    </script>
+
+    <script>
+        $(document).on("change", "#inputPeriode", function() {
+            if ($(this).val() == 'bulanan') {
+                $("#harian").hide();
+                $("#bulanan").show();
+            } else if ($(this).val() == 'harian') {
+                $("#harian").show();
+                $("#bulanan").hide();
+            } else {
+                $("#harian").hide();
+                $("#bulanan").hide();
+            }
+        });
     </script>
 
 </body>
