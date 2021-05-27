babe5a21f9283d62cd0988a6895f92ac3117ea9d
Musthafa Faishal <musthafafaishal@gmail.com>
2021-05-28 01:25:04 +0700

    Update

--- app/Views/laporan/index.php
+++ app/Views/laporan/index.php
@@ -13,6 +13,10 @@
             <!-- Judul -->
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                 <h1 class="h2">Laporan</h1>
+                <div class="btn btn-sm btn-outline-secondary">
+                    <span data-feather="calendar"></span>
+                    <?php echo date('l, d F Y'); ?>
+                </div>
             </div>
             <!-- Akhir Judul -->
 
--- app/Views/pengguna/index.php
+++ app/Views/pengguna/index.php
@@ -15,6 +15,10 @@
             <!-- Judul -->
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                 <h1 class="h2">Pengguna</h1>
+                <div class="btn btn-sm btn-outline-secondary">
+                    <span data-feather="calendar"></span>
+                    <?php echo date('l, d F Y'); ?>
+                </div>
             </div>
             <!-- Akhir Judul -->
 
